a5b672a0081b646d03bac81efec488d1f53d7026
Chocotunda <kdgganpat@gmail.com>
2019-03-04 16:23:04 +0100

    added dummy dog

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        <div>
+          <img
+            src="https://static.valentinosdisplays.com/originalimages/medium/2741-dog-mannequin-uk.jpg"
+            alt="dummy dog"
+          />
+        </div>
       </div>
     );
   }
