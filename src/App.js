86393230f2295b4cccb77f466b69546c5f8e3a91
Chocotunda <kdgganpat@gmail.com>
2019-03-04 17:31:27 +0100

    redux setup works with extensions

--- src/App.js
+++ src/App.js
@@ -22,3 +22,6 @@ class App extends Component {
 }
 
 export default App;
+
+
+
--- src/store.js
+++ src/store.js
@@ -1,6 +1,9 @@
-import { createStore } from 'redux'
-import reducer from './reducer'
+import { createStore } from "redux";
+import reducer from "./reducer";
 
-const store = createStore(reducer)
+const enhancer =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-export default store
+const store = createStore(reducer, enhancer);
+
+export default store;
