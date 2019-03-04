64b9c695b12e94a7153a020e202ee70ecd10903f
Bencilki <alpertunca@gmail.com>
2019-03-04 18:04:50 +0100

    got some error on fetching data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import DogBreedImagesContainer from './DogBreedImagesContainer';
+import DogsList from './components/DogsList';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <DogBreedImagesContainer />
+       <h1><DogsList DogsList=''/>
+       </h1>
       </div>
     );
   }
