a244611989c39e7fc8f3bc012fa5a5274b1746fe
Maudy <maudymorien@hotmail.nl>
2019-03-05 11:44:16 +0100

    made the list max 3

--- src/AnswerList.js
+++ src/AnswerList.js
@@ -5,19 +5,29 @@ export default class AnswerList extends Component {
     return <li key={breed}>{breed}
     </li>
   }
+
   render() {
     const { dogBreeds } = this.props
+    const limit = 3
+    // const random = (array) => {
+      // return array[Math.floor(Math.random() * array.length)]
+    // }
 
+    const maxThree = () => {
+
+      if (!dogBreeds) {
+        return 'Loading...'
+      } else {
+        return <ul>{dogBreeds.slice(0, limit).map(this.renderDogBreed)}</ul>
+      }
+    }
+    //  [Math.floor(Math.random() * dogBreeds.length)]
     return (
       <div className="dogs-list">
         <h1>What breed am I?</h1>
-
-        {!dogBreeds && 'Loading...'}
-
-        {dogBreeds &&
-          <ul>{dogBreeds.map(this.renderDogBreed)}</ul>
-        }
+        {maxThree()}
       </div>
     )
   }
 }
+
