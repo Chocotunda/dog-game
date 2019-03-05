791e8c8593ccc92989f59d97e3aea86cecf3f982
Chocotunda <kdgganpat@gmail.com>
2019-03-05 14:01:56 +0100

    shows random single breed

--- src/AnswerList.js
+++ src/AnswerList.js
@@ -1,30 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class AnswerList extends Component {
   renderDogBreed(breed) {
-    return <li key={breed}>{breed}
-    </li>
+    return <li key={breed}>{breed}</li>;
   }
 
   render() {
-    const { dogBreeds } = this.props
-    const shuffleArray = require('shuffle-array')
-    console.log(dogBreeds)
-    const limit = 3
+    const { dogBreeds } = this.props;
+    // const limit = 3;
+    // const random = (array) => {
+    // return array[Math.floor(Math.random() * array.length)]
+    // }
+
+    //Math.floor(Math.random() *
+
     const maxThree = () => {
       if (!dogBreeds) {
-        return 'Loading...'
-      } shuffleArray(dogBreeds)
-        return <ul>{dogBreeds.slice(0, limit).map(this.renderDogBreed)}</ul>
-      
-    }
+        return "Loading...";
+      }
+      let ranDogNum = Math.floor(Math.random() * dogBreeds.length);
+      return <ul>{dogBreeds[ranDogNum]}</ul>;
 
+      // else {
+      //   return <ul>{dogBreeds.slice(0, limit).map(this.renderDogBreed)}</ul>;
+      // }
+    };
+    //  [Math.floor(Math.random() * dogBreeds.length)]
     return (
       <div className="dogs-list">
         <h1>What breed am I?</h1>
         {maxThree()}
       </div>
-    )
+    );
   }
 }
-
