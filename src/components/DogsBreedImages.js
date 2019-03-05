c778142901dd6ffa575b1ea39d88c04f659baeb2
Chocotunda <kdgganpat@gmail.com>
2019-03-05 15:24:43 +0100

    cleanup

--- src/components/DogsBreedImages.js
+++ src/components/DogsBreedImages.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class DogsBreedImages extends Component {
-
   renderDogBreed(breed) {
-    return <li key={breed}>
-      
-      <img src = {breed} alt={breed} />
-      
-    </li>
+    return (
+      <li key={breed}>
+        <img src={breed} alt={breed} />
+      </li>
+    );
   }
 
   render() {
-    const { dogBreeds } = this.props
-    const sliceNumber = Math.floor(Math.random() * 5)    
+    const { dogBreeds } = this.props;
+    const sliceNumber = Math.floor(Math.random() * 5);
     return (
       <div className="dogs-images">
         <h1>Breaking Breeds</h1>
 
-        {!dogBreeds && 'Loading...'}
+        {!dogBreeds && "Loading..."}
 
-        {dogBreeds &&
-        <ul>
-          {dogBreeds
-            .slice(sliceNumber, sliceNumber+1)
-            .map(this.renderDogBreed)
-          }
-        </ul>
-        }
+        {dogBreeds && (
+          <ul>
+            {dogBreeds
+              .slice(sliceNumber, sliceNumber + 1)
+              .map(this.renderDogBreed)}
+          </ul>
+        )}
       </div>
-    )
+    );
   }
 }
