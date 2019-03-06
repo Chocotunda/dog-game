646e0c26f6d1ab4ec9b695b80ce4491c9c78099d
Chocotunda <kdgganpat@gmail.com>
2019-03-06 15:56:10 +0100

    rewired answerList

--- src/components/AnswerListContainer.js
+++ src/components/AnswerListContainer.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
-import AnswerList from "./AnswerList";
+import Shuffle from "./Shuffle";
 
-export default class AnswerListContainer extends Component {
-
+export default class AnswerList extends Component {
   render() {
-    console.log("PROPSSSS", this.props.breedList)
-    return <AnswerList dogBreeds={this.props.breedList} />;
+    console.log(this.props.breedList);
+
+    const { breedList } = this.props;
+
+    const optionalAnswer = () => {
+      if (!breedList) {
+        return "Loading...";
+      }
+      let ranDogNum = Math.floor(Math.random() * breedList.length);
+      return <ul>{breedList[ranDogNum]}</ul>;
+    };
+
+    return (
+      <Shuffle
+        shuffle1={optionalAnswer()}
+        shuffle2={optionalAnswer()}
+        shuffle3={optionalAnswer()}
+      />
+    );
   }
 }
