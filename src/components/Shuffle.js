c7a593edad9ae5d68ea4d6ff9de32fd3cb23362b
Chocotunda <kdgganpat@gmail.com>
2019-03-06 15:32:38 +0100

    fixed first anwer

--- src/components/Shuffle.js
+++ src/components/Shuffle.js
@@ -1,44 +1,28 @@
 import React, { Component } from "react";
 import shuffleArray from "shuffle-array";
 
-
 export default class Shuffle extends Component {
-  handleClick = (value) => {
-    if (value === "true") {
-      return this.goodAnswer(), this.progressBar()
-    } else {
-      return this.badAnswer()
-    }
-  }
-
-  progressBar = () => {
-    console.log('making progress')
-  }
-
-  goodAnswer = () => {
-    console.log('you are correct!')
-    // show next question with render
-  }
-
-  badAnswer = () => {
-    console.log('Whoops, wrong answer')
-    // timeOut 2 sec && goodAnswer
-  }
+  handleClick = value => {
+    return console.log("ouch");
+  };
 
   render() {
-    const wrongAnswer = this.props.shuffle1
-    const anotherWrongAnswer = this.props.shuffle2
-    const rightAnswer = this.props.shuffle3
+    const wrongAnswer = this.props.shuffle1;
+    const anotherWrongAnswer = this.props.shuffle2;
+    const rightAnswer = this.props.shuffle3;
 
-    const list = [wrongAnswer, anotherWrongAnswer, rightAnswer]
-    console.log(list, 'normal')
-    shuffleArray(list)
+    const list = [wrongAnswer, anotherWrongAnswer, rightAnswer];
+    console.log(list, "normal");
+    shuffleArray(list);
 
-
-    return <ul>
-      <li onClick={() => this.handleCLick(wrongAnswer)}>{wrongAnswer}</li>
-      <li onClick={() => this.handleClick(anotherWrongAnswer)}>{anotherWrongAnswer}</li>
-      <li onClick={() => this.handleClick(rightAnswer)}>{rightAnswer}</li>
-    </ul>
+    return (
+      <ul>
+        <li onClick={() => this.handleClick(wrongAnswer)}>{wrongAnswer}</li>
+        <li onClick={() => this.handleClick(anotherWrongAnswer)}>
+          {anotherWrongAnswer}
+        </li>
+        <li onClick={() => this.handleClick(rightAnswer)}>{rightAnswer}</li>
+      </ul>
+    );
   }
 }
