73a476f1afff8dc17505dfd7356af48ffcc6f93b
Chocotunda <kdgganpat@gmail.com>
2019-03-07 12:05:03 +0100

    1st try

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,12 +8,28 @@ const breedReducer = () => {
   ];
 };
 
+const initialState = {
+  currentBreeds: null,
+  correctAnswer: null,
+  correct: null,
+}
+
 const selectedBreedReducer = (selectedBreed = null, action) => {
   if (action.type === "BREED_SELECTED") {
     return action.payload;
   }
   return selectedBreed;
 };
+
+const remainingBreeds = (state = null, action){
+  // deals with breed we are not using -> store 84 breeds here (and leave them for now)
+}
+
+
+
+const game = (state = initialState, action){
+  // 
+}
 
 export default combineReducers({
   breed: breedReducer,
